9c994a431a6f3d65593d4a411aeff628a5f24d30
Yan Rodrigues <yan.rodriguescs@outlook.com>
2026-04-27 20:28:55 -0300

    feat: setup logging instance

--- apps/solver/src/main.py
+++ apps/solver/src/main.py
@@ -1,5 +1,18 @@
+from logger import setup_logger, get_logger
+
+logger = get_logger()
+
+
 def main():
-    print("Hello, World!")
+    setup_logger(
+        log_dir="logs",
+        log_level="INFO",
+        diagnose=False,
+        rotation="25 MB",
+        retention="7 days",
+    )
+
+    logger.info("Starting solver application")
 
 
 if __name__ == "__main__":
